11d8fe9a234fef93abf94e3775dabc550d8688aa
Michael Loftis <mloftis@wgops.com>
2022-04-10 20:15:53 +0100

    tag as 0.5.9

--- OKP1 Stationeers Editor/Properties/AssemblyInfo.cs
+++ OKP1 Stationeers Editor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TCNet Systems")]
 [assembly: AssemblyProduct("OKP1 Stationeers Editor")]
-[assembly: AssemblyCopyright("Copyright ©  2017-2021")]
+[assembly: AssemblyCopyright("Copyright ©  2017-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.8.0")]
-[assembly: AssemblyFileVersion("0.5.8.0")]
+[assembly: AssemblyVersion("0.5.9.0")]
+[assembly: AssemblyFileVersion("0.5.9.0")]
